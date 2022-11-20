3cdef08db3947b8db91d4181c5cba7e73bf18f1e
nissano <nissano@wix.com>
2022-11-21 00:58:12 +0200

    5-html-page

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const http = require("http"); // build in in node js 🤓
 
 const server = http.createServer((req, res) => {
   // We are getting request, and retturing a response.
+
+  // We want to send some HTML! :)
+  res.setHeader("Content-Type", "text/html");
+  res.write("<h1>Pickle Rick!!!</h2>");
+  res.end();
 });
 
 server.listen(4000, () => console.log("Server is running!"));
